2036d9f2168e0f8d3b2eb0bcac7eeea06defe011
D3Catalyst <d3.catalyst@gmail.com>
2014-07-27 01:21:13 -0500

    Fix issue with global var HTTP_CLIENT in GeoIp.php

--- src/D3Catalyst/GeoIP/GeoIP.php
+++ src/D3Catalyst/GeoIP/GeoIP.php
@@ -221,7 +221,7 @@ class GeoIP{
     */
     private function getClientIp() {
         $ipaddress = '';
-        if ($_SERVER['HTTP_CLIENT_IP'])
+        if (isset($_SERVER['HTTP_CLIENT_IP']) && $_SERVER['HTTP_CLIENT_IP'])
             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
         else if($_SERVER['HTTP_X_FORWARDED_FOR'])
             $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
