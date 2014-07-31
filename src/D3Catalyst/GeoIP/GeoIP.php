2a20abfddc354032d9d4dc243543bffd7b7f70a0
D3Catalyst <d3.catalyst@gmail.com>
2014-07-31 05:00:36 -0500

    Change url in curl invocation

--- src/D3Catalyst/GeoIP/GeoIP.php
+++ src/D3Catalyst/GeoIP/GeoIP.php
@@ -197,10 +197,8 @@ class GeoIP{
       
       $url      = 'http://ip-api.com/json/'.$ip;
       $timeout  = 30; // Timeout
-      $data     = NULL;
       
-      $ch = curl_init();
-      curl_setopt ($ch, CURLOPT_URL, $url);
+      $ch = curl_init($url);
       curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
       curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
       
